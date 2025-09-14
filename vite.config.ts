2434e58d36368c17178dea180c324fcf436df95c
Cody Seibert <webdevcody@gmail.com>
2025-09-13 23:07:31 -0400

    Update Vite configuration to include viteReact plugin and add SSR settings for react-dropzone.

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,18 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
+import viteReact from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  return {
-    server: {
-      port: 3000,
-    },
-    plugins: [
-      tsConfigPaths(),
-      tailwindcss(),
-      tanstackStart({ target: "node-server", customViteReactPlugin: true }),
-    ],
-  };
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  ssr: { noExternal: ["react-dropzone"] },
+
+  plugins: [
+    tsConfigPaths(),
+    tailwindcss(),
+    tanstackStart({ target: "node-server", customViteReactPlugin: true }),
+    viteReact(),
+  ],
 });
